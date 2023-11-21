86db17d96773217fb24bdc31e1bff1d6b2509467
unix-waltz <riosraskaa@gmail.com>
2023-11-21 09:38:07 +0700

    adding controller

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import express from "express";
 const app = express();
 import env from "dotenv";
+import clientRoute from './src/Routes/client-route.js'
+import adminRoute from './src/Routes/admin-route.js'
 env.config();
 app.set('view engine', 'ejs');
 app.use(express.static('./public'));
-import clientRoute from './src/Routes/client-route.js'
+
 app.get('/', (req, res) => {
     res.redirect('/client/')
 })
 app.use('/client', clientRoute);
-
+app.use('/admin', adminRoute);
 
 app.listen(process.env.LOCAL_PORT,() =>{
     console.log(`Starting on port ${process.env.LOCAL_PORT}`);
